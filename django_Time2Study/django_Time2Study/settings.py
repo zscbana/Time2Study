920eb436e05645f85926620ef31568e5533d75de
zscbana <zscbana@gmail.com>
2024-10-17 17:12:16 +0300

    User Profile and Picture and signals to automatic add user profile when user is created

--- django_Time2Study/django_Time2Study/settings.py
+++ django_Time2Study/django_Time2Study/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
