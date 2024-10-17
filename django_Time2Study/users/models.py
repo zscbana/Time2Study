94212a1228d7082769ecf89a6d31040aca4a32e2
zscbana <zscbana@gmail.com>
2024-10-17 23:44:05 +0300

    Enhance User Profile: Allow users to edit their profiles and upload a profile photo; update post images functionality.

--- django_Time2Study/users/models.py
+++ django_Time2Study/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image, ExifTags
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +8,34 @@ class Profile(models.Model):
     
     def __str__ (self):
         return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        
+        # Fix EXIF orientation issue
+        try:
+            for orientation in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation] == 'Orientation':
+                    break
+            
+            exif = img._getexif()
+            if exif is not None:
+                orientation_value = exif.get(orientation)
+                if orientation_value == 3:
+                    img = img.rotate(180, expand=True)
+                elif orientation_value == 6:
+                    img = img.rotate(270, expand=True)
+                elif orientation_value == 8:
+                    img = img.rotate(90, expand=True)
+        except (AttributeError, KeyError, IndexError):
+            # No EXIF data, skip orientation handling
+            pass
+        
+        # Resize image if it's too large
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+        
+        img.save(self.image.path)
