7d7abac94dd70a358ef72d724aa0e1274728bccf
zscbana <zscbana@gmail.com>
2024-10-18 20:22:34 +0300

    Pagination

--- django_Time2Study/main/views.py
+++ django_Time2Study/main/views.py
@@ -30,6 +30,7 @@ class PostListView(ListView):
     template_name = 'main/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
