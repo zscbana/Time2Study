423a6286cec21f1969d85851681740e07abd02f8
zscbana <zscbana@gmail.com>
2024-10-18 08:46:23 +0300

    Implement Create, Update, and Delete functionality for posts; add initial static template for study timer

--- django_Time2Study/main/urls.py
+++ django_Time2Study/main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name='main-home'),
+    path('', PostListView.as_view(), name='main-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='main-about'),
     path("roadmap/", views.roadmap, name='main-roadmap'),
     path('study/', views.study, name='main-study')
