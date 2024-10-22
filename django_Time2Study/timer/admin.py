042ebe8e322b5e82a3cc4ea18701decb04055a81
zscbana <zscbana@gmail.com>
2024-10-22 13:45:49 +0300

    StudySessionAdmin: Enable searching by username in admin page

--- django_Time2Study/timer/admin.py
+++ django_Time2Study/timer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import StudySession
 
-# Register your models here.
+class StudySessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'duration', 'created_at')  # Fields to display in the list view
+    search_fields = ('user__username',)  # Enable searching by username
+
+admin.site.register(StudySession, StudySessionAdmin)
